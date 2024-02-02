382869656d2192f0115bad15274f997251fff786
YoDaddyKev <158497892+YoDaddyKev@users.noreply.github.com>
2024-02-02 14:52:08 +0200

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -96,7 +96,7 @@ async def answer(bot, query):
 
 
 def get_reply_markup(query):
-    buttons = [[InlineKeyboardButton('⟳ ꜱᴇᴀʀᴄʜ ᴀɢᴀɪɴ', switch_inline_query_current_chat=query)]]
+    buttons = [[InlineKeyboardButton('𝘚𝘦𝘢𝘳𝘤𝘩 𝘈𝘨𝘢𝘪𝘯', switch_inline_query_current_chat=query)]]
     return InlineKeyboardMarkup(buttons)
 
 
